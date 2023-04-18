9612652ffa2e50e8311f8ea4c11c1e3d1bda972f
fl1pHover <bishu.hover@gmail.com>
2023-04-18 12:42:42 +0800

    ad

---
+++ lib/modules/prime/widgets/prime_animation_container.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:frontend/modules/modules.dart';
+import 'package:get/get.dart';
+
+class PrimeAnimationContainer extends StatelessWidget {
+  const PrimeAnimationContainer(
+      {super.key,
+      required this.child,
+      this.opacity = -1,
+      this.duration = const Duration(seconds: 1)});
+
+  final Duration duration;
+  final double? opacity;
+  final Widget child;
+  @override
+  Widget build(BuildContext context) {
+    final controller = Get.put(PrimeController());
+    return Obx(() => AnimatedPadding(
+          padding: EdgeInsets.symmetric(
+            vertical: controller.fade.value ? 32 : 0,
+          ),
+          duration: duration,
+          child: AnimatedOpacity(
+            opacity: opacity == -1
+                ? controller.fade.value
+                    ? 0
+                    : 1
+                : opacity!,
+            duration: duration,
+            child: child,
+          ),
+        ));
+  }
+}
